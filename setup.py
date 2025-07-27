8e70b5027b9281be4e4581fef3827b315353dc03
Faisal Chap <155081306+Faisal-Chap@users.noreply.github.com>
2025-07-28 00:04:44 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tictactoe',
-    version='1.0',
+    name="tictactoe",
+    version="1.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        # You don't need 'tk' — it's system installed
-    ],
     entry_points={
-        'console_scripts': [
-            'tictactoe = game.gui:main',
+        "console_scripts": [
+            "tictactoe = tic_tac_toe.main:main"
         ],
-    },
-    package_data={
-        '': ['*.desktop', 'assets/*.png']
     },
 )
